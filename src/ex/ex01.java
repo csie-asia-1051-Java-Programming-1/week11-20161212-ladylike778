959126ee95da80a63a57857c02b97ffb63bf100a
ladylike778 <105021037@live.asia.edu.tw>
2016-12-12 16:05:50 +0800

    dgd

--- src/ex/ex01.java
+++ src/ex/ex01.java
@@ -3,8 +3,24 @@ package ex;
  * Date: 2016/12/12
  * Author: 105021***  ???
  */
+import java.util.*;
 public class ex01 {
 	public static void main(String[] args) {
-
+		Scanner scn=new Scanner(System.in);
+		int n=scn.nextInt();
+		for(int i=0;i<n;i++){
+			int T=scn.nextInt();
+			int m=scn.nextInt();
+			int k=scn.nextInt();
+			int data[]=new int[k];
+			for(int j=0;j<k;j++){
+				data[j]=scn.nextInt();
+			}
+		}
+		
+		
+		
+		
+        
 	}
 }
